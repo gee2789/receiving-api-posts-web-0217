7557b1615d253b2445969974440046dd9716d026
gee2789 <geeyung.li@gmail.com>
2017-03-24 19:51:54 -0400

    Done.

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,3 +1,9 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :id, :title, :description
+
+  def create
+    @post = Post.create(post_params)
+    render json: @post, status: 201
+  end
+
 end
